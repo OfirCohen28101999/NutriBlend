20149c19f5915f6717436cbe1a124d96a631a3ac
ofirCohen <ofir2161324@gmail.com>
2024-03-05 10:05:33 +0200

    converted AddRecipeActivity.kt to AddRecipeFragment & deleted buttons in MainActivity in order to start using "real" navigation

--- app/src/main/java/com/example/nutriblend/MainActivity.kt
+++ app/src/main/java/com/example/nutriblend/MainActivity.kt
@@ -7,68 +7,8 @@ import androidx.fragment.app.Fragment
 import com.example.nutriblend.Modules.Recipes.RecipesFragment
 
 class MainActivity : AppCompatActivity() {
-
-    var fragmentOne: RecipesFragment? = null
-    var fragmentTwo: BlueFragment? = null
-    var fragmentThree: BlueFragment? = null
-    var fragmentFour: BlueFragment? = null
-
-    var buttonOne: Button? = null
-    var buttonTwo: Button? = null
-    var buttonThree: Button? = null
-    var buttonFour: Button? = null
-
-    var currentlyDisplayedFragment: Fragment? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        fragmentOne = RecipesFragment()
-        fragmentTwo = BlueFragment.newInstance("two")
-        fragmentThree = BlueFragment.newInstance("three")
-        fragmentFour = BlueFragment.newInstance("four")
-
-        buttonOne = findViewById(R.id.btnMainTabOne)
-        buttonTwo = findViewById(R.id.btnMainTabTwo)
-        buttonThree = findViewById(R.id.btnMainTabThree)
-        buttonFour = findViewById(R.id.btnMainTabFour)
-
-        buttonOne?.setOnClickListener{
-            fragmentOne?.let {
-                displayBlueFragment(it)
-            }
-        }
-
-        buttonTwo?.setOnClickListener{
-            fragmentTwo?.let {
-                displayBlueFragment(it)
-            }
-        }
-
-        buttonThree?.setOnClickListener{
-            fragmentThree?.let {
-                displayBlueFragment(it)
-            }
-        }
-
-        buttonFour?.setOnClickListener{
-            fragmentFour?.let {
-                displayBlueFragment(it)
-            }
-        }
-
-        fragmentOne?.let {
-            displayBlueFragment(it)
-        }
-    }
-    fun displayBlueFragment(fragment: Fragment){
-        val transaction = supportFragmentManager.beginTransaction()
-        transaction.add(R.id.flMainFragment, fragment)
-        currentlyDisplayedFragment?.let {
-            transaction.remove(it)
-        }
-        transaction.addToBackStack("tag")
-        transaction.commit()
-        currentlyDisplayedFragment = fragment
     }
 }
---
+++ app/src/main/java/com/example/nutriblend/Modules/AddRecipe/AddRecipeFragment.kt
@@ -0,0 +1,46 @@
+package com.example.nutriblend.Modules.AddRecipe
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.example.nutriblend.R
+
+class AddRecipeFragment : Fragment() {
+    private var recipeTitleTextBox: EditText? = null
+    private var recipeIngredientsTextBox: EditText? = null
+    private var recipePreparationStepsTextBox: EditText? = null
+    private var saveRecipeBtn: Button? = null
+    private var cancelRecipeBtn: Button? = null
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_add_recipe, container, false)
+        setupUI(view)
+        return view
+    }
+
+    private fun setupUI(view: View) {
+        recipeTitleTextBox = view.findViewById(R.id.recipeTitleTextBox)
+        recipeIngredientsTextBox = view.findViewById(R.id.recipeIngredientsTextBox)
+        recipePreparationStepsTextBox = view.findViewById(R.id.recipePreparationStepsTextBox)
+        saveRecipeBtn = view.findViewById(R.id.saveRecipeBtn)
+        cancelRecipeBtn = view.findViewById(R.id.cancelRecipeBtn)
+
+        cancelRecipeBtn?.setOnClickListener{
+            // TODO: LATER
+        }
+        saveRecipeBtn?.setOnClickListener{onSaveRecipeClicked()}
+    }
+
+    private fun onSaveRecipeClicked() {
+        val recipeTitle = recipeTitleTextBox?.text.toString()
+        Toast.makeText(context, "recipe: $recipeTitle saved successfully", Toast.LENGTH_SHORT).show()
+    }
+}
